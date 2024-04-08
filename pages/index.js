da5e9601f3c1680e54d5f9d9c542deffb35ab0a5
Ana <30495040+aaam8@users.noreply.github.com>
2024-04-09 07:04:25 +0800

    adding facebook

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import Link from 'next/link';
+<div id="fb-root"></div>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v19.0&appId=3629828190637924" nonce="MEwpbLie"></script>
 
 export default function Home() {
     return (
@@ -6,6 +8,29 @@ export default function Home() {
         <h1>Hello, World!</h1>
         <p>genie's coming!</p>
         <Link href="/post"><a>Post an Item</a></Link>
+        <div class="fb-login-button" data-width="" data-size="" data-button-type="" data-layout="" data-auto-logout-link="true" data-use-continue-as="false"></div>
       </div>
     );
   }
+
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '{your-app-id}',
+      cookie     : true,
+      xfbml      : true,
+      version    : '{api-version}'
+    });
+      
+    FB.AppEvents.logPageView();   
+      
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
